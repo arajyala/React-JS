06affc159c6b2020c620beaf94bb1fc39e953866
arajyala <rajyalaxmianupati1999@gmail.com>
2024-02-12 18:19:49 +0530

    Assignment 1

---
+++ Functional_Comp/Func_Comp1.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+function Func_App1(){
+
+    return <h1>Functional Component 1..</h1> ;
+
+}
+export default Func_App1;
---
+++ Functional_Comp/Func_Comp2.js
@@ -0,0 +1,8 @@
+import React from 'react'
+
+function Func_App2(){
+
+    return <h1>Functional Component 2..</h1>;
+}
+
+export default Func_App2;
